39c14c7fbd3a518553fa304decc2167fb5bffed4
Plamen Kostadinov <kostapla@starsgroup.com>
2020-10-31 21:19:26 +0200

    Complete item unit and integration tests

--- rest_api_testing/part_two/starter_code/tests/unit/models/test_item.py
+++ rest_api_testing/part_two/starter_code/tests/unit/models/test_item.py
@@ -1,19 +1,21 @@
-from unittest import TestCase
-
 from models.item import ItemModel
+from tests.unit.models.unit_base_test import UnitBaseTest
 
 
-class ItemTest(TestCase):
+class ItemTest(UnitBaseTest):
     def test_create_item(self):
-        item = ItemModel('test', 19.99)
+        item = ItemModel('test', 19.99, 1)
 
         self.assertEqual(item.name, 'test',
                          "The name of the item after creation does not equal the constructor argument.")
         self.assertEqual(item.price, 19.99,
                          "The price of the item after creation does not equal the constructor argument.")
+        self.assertEqual(item.store_id, 1,
+                         "The store id of the item after creation does not equal the constructor argument.")
+        self.assertIsNone(item.store, "The store of the item after creation should be none.")
 
     def test_item_json(self):
-        item = ItemModel('test', 19.99)
+        item = ItemModel('test', 19.99, 1)
         expected = {
             'name': 'test',
             'price': 19.99
